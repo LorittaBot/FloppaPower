6406dd2a3f4f1590ea8294da70f032baddd05a0b
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-11-23 13:44:29 -0300

    Whoops, forgot the Serializable annotations

--- src/main/kotlin/net/perfectdreams/floppapower/data/BlockedAvatarHashResponse.kt
+++ src/main/kotlin/net/perfectdreams/floppapower/data/BlockedAvatarHashResponse.kt
@@ -1,5 +1,8 @@
 package net.perfectdreams.floppapower.data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class BlockedAvatarHashResponse(
     val avatarHash: String,
     val entry: Long,
--- src/main/kotlin/net/perfectdreams/floppapower/data/BlockedUserResponse.kt
+++ src/main/kotlin/net/perfectdreams/floppapower/data/BlockedUserResponse.kt
@@ -1,5 +1,8 @@
 package net.perfectdreams.floppapower.data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class BlockedUserResponse(
     val userId: Long,
     val entry: Long,

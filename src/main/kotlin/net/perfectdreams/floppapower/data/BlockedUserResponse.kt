be65c270492d81e912390f714133c023fd4569ec
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-11-28 22:13:12 -0300

    Serialize longs as strings because JavaScript has issues with rounding longs

--- src/main/kotlin/net/perfectdreams/floppapower/data/BlockedUserResponse.kt
+++ src/main/kotlin/net/perfectdreams/floppapower/data/BlockedUserResponse.kt
@@ -1,9 +1,11 @@
 package net.perfectdreams.floppapower.data
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.LongAsStringSerializer
 
 @Serializable
 data class BlockedUserResponse(
+    @Serializable(LongAsStringSerializer::class)
     val userId: Long,
     val entry: Long,
     val valid: Boolean
--- src/main/kotlin/net/perfectdreams/floppapower/data/User.kt
+++ src/main/kotlin/net/perfectdreams/floppapower/data/User.kt
@@ -1,9 +1,11 @@
 package net.perfectdreams.floppapower.data
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.LongAsStringSerializer
 
 @Serializable
 data class User(
+    @Serializable(LongAsStringSerializer::class)
     val id: Long,
     val name: String,
     val discriminator: String,

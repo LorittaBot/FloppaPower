f4bb43018b59344624a1a15617a7f83214a1933f
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-12-04 00:32:02 -0300

    Add more filters to the top mutual users query, sort by account creation date too

--- src/main/kotlin/net/perfectdreams/floppapower/commands/impl/TopMutualUsersCommand.kt
+++ src/main/kotlin/net/perfectdreams/floppapower/commands/impl/TopMutualUsersCommand.kt
@@ -26,9 +26,17 @@ class TopMutualUsersCommand(private val shardManager: ShardManager) : AbstractSl
             val lines = mutableListOf("Users:")
 
             mutualGuilds.toList()
-                .sortedByDescending {
-                    it.second.size
-                }
+                .sortedWith(
+                    compareBy(
+                        {
+                            // negative because we want it to be descending
+                            -it.second.size
+                        },
+                        {
+                            it.first.timeCreated
+                        }
+                    )
+                )
                 .take(MAX_USERS_PER_LIST)
                 .forEach {
                     lines.addAll(generateUserInfoLines(shardManager, it.first, it.second).first)
@@ -42,18 +50,23 @@ class TopMutualUsersCommand(private val shardManager: ShardManager) : AbstractSl
         val userCacheSize = shardManager.userCache.size()
         shardManager.userCache.forEach {
             if (idx % 250_000 == 0 && idx != 0)
-                hook.editOriginal("**Usuários verificados:** $idx/$userCacheSize <a:floppaTeeth:849638419885195324>\nResultado apenas possui os top $MAX_USERS_PER_LIST usuários como também ignora bots!")
+                hook.editOriginal("**Usuários verificados:** $idx/$userCacheSize <a:floppaTeeth:849638419885195324>\nResultado apenas possui os top $MAX_USERS_PER_LIST usuários, ignorando bots e usuários que estão na EPF!")
                     .retainFiles(listOf()) // Remove all files from the message
                     .addFile(generateTopUsersMutualGuildsLines().joinToString("\n").toByteArray(Charsets.UTF_8), "users.txt")
                     .queue()
 
             // Ignore bots
-            if (!it.isBot)
-                mutualGuilds[it] = it.mutualGuilds
+            if (!it.isBot) {
+                val userMutualGuilds = it.mutualGuilds
+                // Ignore users that are in the EPF guild
+                if (!userMutualGuilds.any { it.idLong == Constants.ELITE_PENGUIN_FORCE_GUILD_ID })
+                    mutualGuilds[it] = userMutualGuilds
+            }
+
             idx++
         }
 
-        hook.editOriginal("**Todos os $userCacheSize usuários foram verificados!** <a:SCfloppaEARflop2:750859905858142258>\nResultado apenas possui os top $MAX_USERS_PER_LIST usuários como também ignora bots!")
+        hook.editOriginal("**Todos os $userCacheSize usuários foram verificados!** <a:SCfloppaEARflop2:750859905858142258>\nResultado apenas possui os top $MAX_USERS_PER_LIST usuários, ignorando bots e usuários que estão na EPF!")
             .retainFiles(listOf()) // Remove all files from the message
             .addFile(generateTopUsersMutualGuildsLines().joinToString("\n").toByteArray(Charsets.UTF_8), "users.txt")
             .queue()

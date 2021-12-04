d52b28dfddc6d90e1f3d1d5f5ef2c28f5abade62
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-12-03 23:55:39 -0300

    Register commands after the EPF guild is loaded

--- src/main/kotlin/net/perfectdreams/floppapower/listeners/SlashCommandListener.kt
+++ src/main/kotlin/net/perfectdreams/floppapower/listeners/SlashCommandListener.kt
@@ -2,9 +2,14 @@ package net.perfectdreams.floppapower.listeners
 
 import kotlinx.coroutines.sync.Mutex
 import mu.KotlinLogging
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData
 import net.dv8tion.jda.api.sharding.ShardManager
 import net.perfectdreams.floppapower.FloppaPower
 import net.perfectdreams.floppapower.commands.impl.CheckGuildCommand
@@ -15,6 +20,7 @@ import net.perfectdreams.floppapower.commands.impl.SameAvatarHashCommand
 import net.perfectdreams.floppapower.commands.impl.SameAvatarUserCommand
 import net.perfectdreams.floppapower.commands.impl.SharedGuildsCommand
 import net.perfectdreams.floppapower.commands.impl.SusJoinsCommand
+import net.perfectdreams.floppapower.utils.Constants
 import net.perfectdreams.floppapower.utils.FloppaButtonClickEvent
 import java.util.*
 
@@ -35,6 +41,81 @@ class SlashCommandListener(private val m: FloppaPower, private val shardManager:
         SameAvatarUserCommand(m, shardManager),
         SusJoinsCommand(m, shardManager)
     )
+
+    override fun onGuildReady(event: GuildReadyEvent) {
+        if (event.guild.idLong != Constants.ELITE_PENGUIN_FORCE_GUILD_ID)
+            return
+        
+        val guild = event.guild
+
+        guild.upsertCommand(
+            CommandData("guilds", "Mostra quais servidores o Floppa está")
+        ).queue()
+
+        guild.upsertCommand(
+            CommandData("checkguild", "Verifique meliantes em um servidor")
+                .addOption(OptionType.STRING, "guild_id", "ID do Servidor", true)
+        ).queue()
+
+        guild.upsertCommand(
+            CommandData("checkguilds", "Verifique meliantes em todos os servidores que eu estou")
+        ).queue()
+
+        guild.upsertCommand(
+            CommandData("checksimilaravatars", "Verifique meliantes com avatares similares")
+                .addOption(OptionType.INTEGER, "page", "Página", false)
+        ).queue()
+
+        guild.upsertCommand(
+            CommandData("sameavatar", "Verifique meliantes que possuem o mesmo avatar")
+                .addSubcommands(
+                    SubcommandData("hash", "Verifique meliantes que possuem o mesmo avatar pelo hash")
+                        .addOption(OptionType.STRING, "hash", "O hash do avatar", true),
+                    SubcommandData("user", "Verifique meliantes que possuem o mesmo avatar pelo usuário")
+                        .addOption(OptionType.USER, "user", "O meliante", true)
+                )
+        ).queue()
+
+        guild.upsertCommand(
+            CommandData("sharedguilds", "Verifique aonde os meliantes estão na fuga")
+                .addOption(OptionType.USER, "user", "O meliante", true)
+        ).queue()
+
+        guild.upsertCommand(
+            CommandData("searchusers", "Busca usuários usando um RegEx")
+                .addOption(OptionType.STRING, "pattern", "RegEx pattern do nome de usuário que você deseja procurar", true)
+        ).queue()
+
+        guild.upsertCommand(
+            CommandData("topmutualusers", "Mostra os top usuários que compartilham mais servidores com o Floppa")
+        ).queue()
+
+        guild.upsertCommand(
+            CommandData("susjoins", "Verifique meliantes que entraram em vários servidores em seguida")
+                .addOptions(
+                    OptionData(OptionType.STRING, "time", "A diferença + e - de tempo que o meliante entrou, baseado no tempo de entrada no \"meio\" dos joins", true)
+                        .addChoice("10 segundos", "5")
+                        .addChoice("15 segundos", "7")
+                        .addChoice("30 segundos", "15")
+                        .addChoice("Um minuto", "30")
+                        .addChoice("Dois minutos", "60")
+                        .addChoice("Cinco minutos", "150")
+                        .addChoice("Dez minutos", "300")
+                        .addChoice("Trinta minutos", "900")
+                )
+                .addOptions(
+                    OptionData(OptionType.STRING, "creation_time_filter", "Filtrar usuários que criaram a conta recentemente, acelera a pesquisa de usuários", true)
+                        .addChoice("3 dias", "3")
+                        .addChoice("7 dias", "7")
+                        .addChoice("14 dias", "14")
+                        .addChoice("30 dias", "30")
+                        .addChoice("90 dias", "90")
+                        .addChoice("180 dias", "180")
+                        .addChoice("365 dias", "365")
+                        .addChoice("Gosto de ver pegando fogo!", "36500")
+                )
+        ).queue()
+    }
 
     override fun onSlashCommand(event: SlashCommandEvent) {
         logger.info { "Received Slash Command ${event.name} from ${event.user}" }
